0be6d90c38d649fa53561b936971150dbcb96f3e
archan-sureja <archan.sureja@simfomsolutions.com>
2026-02-03 18:57:10 +0530

    added addNewTask function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function addNewTask(event){
+function addNewTask(event) {
     event.preventDefault()
     let title = event.target.title.value
     let description = event.target.description.value
-    let template = `<div class="task">
+    let template = `<div class="task" draggable="true">
                 <h4>${title}</h4>
                 <p>${description}</p>
             </div>`
-    let pending_tasks = document.getElementById("")
+    let pending_tasks = document.getElementById("pending")
+    pending_tasks.insertAdjacentHTML("beforeend", template)
 }
