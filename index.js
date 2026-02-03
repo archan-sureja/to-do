0bb4a39b98b65972dfd75116bf41c7066f51a186
archan-sureja <archan.sureja@simfomsolutions.com>
2026-02-03 18:05:34 +0530

    finished simple structure

---
+++ index.js
@@ -0,0 +1,10 @@
+function addNewTask(event){
+    event.preventDefault()
+    let title = event.target.title.value
+    let description = event.target.description.value
+    let template = `<div class="task">
+                <h4>${title}</h4>
+                <p>${description}</p>
+            </div>`
+    let pending_tasks = document.getElementById("")
+}
