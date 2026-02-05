05db91338b60f2cb1ddedddf15c62207b0b2dd19
archan-sureja <archan.sureja@simfomsolutions.com>
2026-02-05 17:23:54 +0530

    added localstorage support

--- index.js
+++ index.js
@@ -8,31 +8,65 @@ function addNewTask(event) {
                 <p>${description}</p>
             </div>`
     let pending_tasks = document.getElementById("pending")
+    let task = {
+        id: task_id,
+        template: template,
+        status: "pending"
+    }
+    storeTask(task)
     pending_tasks.insertAdjacentHTML("beforeend", template)
 }
 
-function handleDragStart(event){
-    event.dataTransfer.setData("text",event.target.id)
+function handleDragStart(event) {
+    event.dataTransfer.setData("text", event.target.id)
 
 }
 
-function getUniqueID(){
+function getUniqueID() {
     let id = localStorage.getItem("unique_id")
-    if(!id){
+    if (!id) {
         id = "0"
     }
-    id = parseInt(id,10)
-    localStorage.setItem("unique_id",id+1)
+    id = parseInt(id, 10)
+    localStorage.setItem("unique_id", id + 1)
     return id
 }
 
-function handleDragOver(event){
+function handleDragOver(event) {
     event.preventDefault()
 }
 
-function handleDrop(event){
+function handleDrop(event) {
     event.preventDefault()
     let task_id = event.dataTransfer.getData("text")
+    updateTaskStatus(task_id,event.target.id)
     event.target.appendChild(document.getElementById(task_id))
 
+}
+function handleOnLoad() {
+    let task_list = JSON.parse(localStorage.getItem("task_list"))
+    if (!task_list) {
+        task_list = {}
+        return
+    }
+    for(let key in task_list){
+        let col = document.getElementById(task_list[key].status)
+        col.insertAdjacentHTML("beforeend",task_list[key].template)
+    }
+}
+
+function updateTaskStatus(task_id,status){
+    let task_list = JSON.parse(localStorage.getItem("task_list"))
+    task_list[task_id].status = status
+    localStorage.setItem("task_list",JSON.stringify(task_list))
+}
+function storeTask(task) {
+    let task_list = localStorage.getItem("task_list")
+    if (!task_list) { task_list = "{}" }
+    task_list = JSON.parse(task_list)
+    task_list[task.id] = task
+    localStorage.setItem("task_list", JSON.stringify(task_list))
+}
+
+function removeAllTasks(){
 }
